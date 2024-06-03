c829cc86a8d1e8f646c74de7a202cf319e752e4f
Nirmalya Nayak <n.nayak263@gmail.com>
2024-06-03 19:45:36 +0530

    add POST: api/v1/todos to create todo

--- main.go
+++ main.go
@@ -15,8 +15,27 @@ type ToDo struct {
 func main() {
 	app := fiber.New()
 
+	todos := []ToDo{}
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(200).JSON(fiber.Map{"message": "Hello, World! 🚀"})
+	})
+
+	app.Post("/api/v1/todos", func(c *fiber.Ctx) error {
+		todo := &ToDo{}
+
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+
+		if todo.Body == "" {
+			return c.Status(400).JSON(fiber.Map{"error": "ToDo Body is Required!"})
+		}
+
+		todo.ID = len(todos) + 1
+		todos = append(todos, *todo)
+
+		return c.Status(201).JSON(todo)
 	})
 
 	log.Fatal(app.Listen(":8080"))
