e2e4996b6fa4e6bcc2234ff9623a37923db9e1c5
Nirmalya Nayak <n.nayak263@gmail.com>
2024-06-13 11:06:39 +0530

    add new component: ToDoForm

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,14 @@
 import { Container, Stack } from "@chakra-ui/react";
 
 import { Navbar } from "./components/navbar";
+import { ToDoForm } from "./components/todo-form";
 
 export default function App() {
   return (
     <Stack h="100vh">
       <Navbar />
       <Container>
-        {/* ToDoForm */}
+        <ToDoForm />
         {/* ToDoList */}
       </Container>
     </Stack>
