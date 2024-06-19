69b2e2c6c4497fe424e586fbb73d7204f291708e
Nirmalya Nayak <n.nayak263@gmail.com>
2024-06-19 07:42:16 +0530

    add functionality: create todo

--- client/src/components/todo-form.tsx
+++ client/src/components/todo-form.tsx
@@ -1,23 +1,56 @@
 import { useState } from "react";
 import { Button, Flex, Input, Spinner } from "@chakra-ui/react";
 import { IoMdAdd } from "react-icons/io";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { BASE_URL } from "../const";
 
 export function ToDoForm() {
-  const [newTodo, setNewTodo] = useState("");
-  const [isPending, setIsPending] = useState(false);
+  const queryClient = useQueryClient();
 
-  const createTodo = async (e: React.FormEvent) => {
-    e.preventDefault();
-    alert("ToDo Added!");
-  };
+  const [newToDo, setNewToDo] = useState("");
+
+  const { mutate: createToDo, isPending: isCreating } = useMutation({
+    mutationKey: ["createTodo"],
+    mutationFn: async (e: React.FormEvent) => {
+      e.preventDefault();
+      try {
+        const res = await fetch(BASE_URL + `/todos`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ body: newToDo }),
+        });
+        const data = await res.json();
+
+        if (!res.ok) {
+          throw new Error(data.error || "Something Went Wrong! ❌");
+        }
+
+        setNewToDo("");
+
+        return data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+    },
+    onError: (error) => {
+      // TODO: Show Error Toast Notification
+      console.error(error);
+    },
+  });
 
   return (
-    <form onSubmit={createTodo}>
+    <form onSubmit={createToDo}>
       <Flex gap={2}>
         <Input
           type="text"
-          value={newTodo}
-          onChange={(e) => setNewTodo(e.target.value)}
+          value={newToDo}
+          onChange={(e) => setNewToDo(e.target.value)}
           ref={(input) => input && input.focus()}
         />
         <Button
@@ -27,7 +60,7 @@ export function ToDoForm() {
             transform: "scale(.97)",
           }}
         >
-          {isPending ? <Spinner size={"xs"} /> : <IoMdAdd size={30} />}
+          {isCreating ? <Spinner size={"xs"} /> : <IoMdAdd size={30} />}
         </Button>
       </Flex>
     </form>
